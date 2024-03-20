088dfaf5299d770869d89dce911258291350b88e
Turing158 <15818961209@163.com>
2024-03-20 15:36:06 +0800

    test

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,35 +1,25 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore('user', () => {
-  const user = ref("")
-  const name = ref("")
+export const useUserStore = defineStore('user-Sf', () => {
+  const user = ref('')
+  const name = ref('')
   const level = ref(0)
   const exp = ref(0)
   const maxExp = ref(0)
   const gender = ref(0)
-  const brith = ref("")
-  const getUserObject = ()=>{
-    return {
-        user:user.value,
-        name:name.value,
-        level:level.value,
-        exp:exp.value,
-        maxExp:maxExp.value,
-        gender:gender.value,
-        brith:brith.value
-    }
-  }
-  const setUserObject = (user,name,level,exp,maxExp,gender,brith)=>{
-    user.value = user;
-    name.value = name;
-    level.value = level;
-    exp.value = exp;
-    maxExp.value = maxExp;
-    gender.value = gender;
-    brith.value = brith;
-  }
-  
+  const brith = ref('')
 
-  return { user,name,level,exp,maxExp,gender,brith,getUserObject,setUserObject }
+  const setUserObject = (user, name, level, exp, maxExp, gender, brith) => {
+    user.value = user
+    name.value = name
+    level.value = level
+    exp.value = exp
+    maxExp.value = maxExp
+    gender.value = gender
+    brith.value = brith
+  }
+
+  return { user, name, level, exp, maxExp, gender, brith, setUserObject }
 })
+export default useUserStore
--- src/views/content/HomeContent.vue
+++ src/views/content/HomeContent.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <Book>
-            <div class="content">
-                <el-button @click="Click"></el-button>
-            </div>
-        </Book>
-    </div>
+  <div>
+    <Book>
+      <div class="content">
+        <el-button @click="click()"></el-button>
+      </div>
+    </Book>
+  </div>
 </template>
 <script setup>
 import Book from '../../components/Book.vue'
 import useUserStore from '@/stores/user'
-const userStore = useUserStore();
-const click = ()=>{
-
+const userStore = useUserStore()
+const click = () => {
+    userStore.setUserObject("user","name",1,1,2,1,"0000-00-00")
 }
 </script>
