7526caa54f07a913f436f0ad90521cbb3750cc5a
Turing158 <15818961209@163.com>
2024-03-20 15:27:28 +0800

    none

--- src/views/content/HomeContent.vue
+++ src/views/content/HomeContent.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <Book>
-            <div class="content"></div>
+            <div class="content">
+                <el-button @click="Click"></el-button>
+            </div>
         </Book>
     </div>
 </template>
 <script setup>
 import Book from '../../components/Book.vue'
+import useUserStore from '@/stores/user'
+const userStore = useUserStore();
+const click = ()=>{
 
+}
 </script>
