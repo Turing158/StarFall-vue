53bed488a3bb473d8d2760f4002cb451dae533ad
Turing158 <15818961209@163.com>
2024-04-15 16:34:19 +0800

    安装vue-cropper的更改

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import "vue-cropper/dist/index.css";
+
 import App from './App.vue'
 import router from './router'
 
