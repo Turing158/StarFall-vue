ba30f70dac7dcc2595fe4b01797a4f72fa74d7fc
Turing158 <15818961209@163.com>
2024-04-19 16:13:56 +0800

    用户列表可显示，功能未完成

--- src/api/admin/user.js
+++ src/api/admin/user.js
@@ -3,3 +3,9 @@ import request from "@/util/request";
 const urlHead = "/admin/user";
 
 export const findAllUsersForSelect = ()=>request.post(urlHead+"/adminFindAllUsersForSelect")
+
+export const findAllUser = (page)=>{
+    let param = new URLSearchParams();
+    param.append("page",page)
+    return request.post(urlHead+"/adminFindAllUsers",param)
+}
--- src/views/admin/UserView.vue
+++ src/views/admin/UserView.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="user" :class="isDark ? 'dark' : ''">
         <h2>用户管理</h2>
-        <div style="height: 10px"></div>
-        <el-button type="primary" @click="onPage()" plain>添加</el-button>
+        <div style="margin: 10px 0;">
+            <el-button type="primary" @click="onPage()" plain>添加</el-button>
+            <span style="position: relative;font-size: 12px;float: right;top: 20px; right: 50px;">tips: 点击头像可更换头像</span>
+        </div>
         <el-table :data="users" :class="isDark ? 'dark' : ''">
-            <el-table-column label="用户名" prop="user" width="80"/>
-            <el-table-column label="昵称" prop="name"/> 
+            <el-table-column label="用户名" prop="user" fixed/>
+            <el-table-column label="昵称" prop="name" width="200"/>
             <el-table-column label="密码" prop="password"/>
-            <el-table-column label="邮箱" prop="email"/>
-            <el-table-column label="性别" prop="gender">
+            <el-table-column label="邮箱" prop="email" width="200"/>
+            <el-table-column label="性别" prop="gender" width="120">
                 <template #default="{row}">
                     {{ 
                         row.gender == 0 ? 
@@ -21,21 +23,34 @@
                     }}
                 </template>
             </el-table-column>
-            <el-table-column label="生日" prop="birthday"/>
+            <el-table-column label="生日" prop="birthday" width="120"/>
             <el-table-column label="经验" prop="exp"/>
             <el-table-column label="等级" prop="level"/>
-            <el-table-column label="头像" prop="avatar"/>
-            <el-table-column width="300" align="center">
+            <el-table-column label="头像" prop="avatar" align="center">
+                <template #default="{row}">
+                    <img :src="'../src/assets/avatar/'+row.avatar" style="width: 40px; height: 40px; border-radius: 50%;border: 2px solid #999;"/>
+                    <input type="file" class="selectImg"/>
+                </template>
+            </el-table-column>
+            <el-table-column width="200" align="center" fixed="right">
                 <template #header>
                     <el-input v-model="search" size="small" placeholder="Type to search" />
                 </template>
                 <template #default="{row}">
                     <el-button type="primary" plain @click="onPage(row)" >编辑</el-button>
                     <el-button type="danger" plain @click="onDel(row)">删除</el-button>
-                    <el-button type="warning" plain @click="setttingAvatar()">修改头像</el-button>
                 </template>
             </el-table-column>
         </el-table>
+        <div class="page">
+            <el-pagination
+            @current-change="handleCurrentChange"
+            :current-page="page"
+            :page-size="10"
+            layout="total, prev, pager, next"
+            background
+            :total="total"/>
+        </div>
         <el-dialog v-model="infoPage" width="600" :close-on-click-modal="false" :style="{'--el-dialog-bg-color':isDark ? '#2b2b2b' : '#fff','--el-text-color-primary':isDark ? '#dedede' : '#131313'}">
             <el-form inline>
                 <el-form-item label="用户名">
@@ -73,30 +88,28 @@
                 </el-form-item>
             </el-form>
             <template #footer>
-                <el-button @click="clear()" plain>取 消</el-button>
+                <el-button type="danger" @click="clear()" plain>取 消</el-button>
                 <el-button type="primary" @click="confirm()" plain>确 定</el-button>
             </template>
         </el-dialog>
     </div>
 </template>
 <script setup>
+import { findAllUser } from '@/api/admin/user';
 import { ElMessage, ElMessageBox } from 'element-plus';
-import {inject,reactive, ref} from 'vue'
+import {inject,onMounted,reactive, ref} from 'vue'
 const isDark = inject('isDark')
-const users = ref([
-    {
-        user: 'admin',
-        password: '***',
-        email: '',
-        name : 'admin',
-        gender: 3,
-        birthday: '2021-10-01',
-        exp: 100,
-        level: 10,
-        avatar: 'default.png',
-        role: ''
-    }
-])
+const users = ref([])
+
+
+const page = ref(1)
+const total = ref(0)
+const handleCurrentChange = (e)=>{
+    page.value = e
+    getUserList()
+}
+
+
 const user = reactive({
         user: '',
         password: '',
@@ -174,11 +187,44 @@ const confirm = ()=>{
     
 }
 const setttingAvatar = ref()
+const getUserList = async()=>{
+    await findAllUser(page.value).then(res=>{
+        users.value = res.data.object
+        total.value = res.data.num
+    }).catch(err=>{
+        ElMessage.error('获取用户列表失败')
+    })
+}
+const init = () =>{
+    getUserList()
+}
+onMounted(init)
 </script>
 <style scoped>
 .user{
     width: calc(100% - 40px);
     margin: 20px;
+}
+.selectImg{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 45px;
+    height: 45px;
+    background-color: #222;
+    margin-top: 8px;
+    margin-left: 18px;
+    border-radius: 50%;
+    opacity: 0;
+    cursor: pointer;
+}
+.selectImg::-webkit-file-upload-button{
+    display: none;
+}
+.page{
+    margin-top: 20px;
+    display: flex;
+    justify-content: end;
 }
 .dark{
     --el-table-tr-bg-color: #2b2b2b;
